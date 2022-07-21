e702213b771c70870f04306acaae7f75aae704ef
Alexey Berlev <ABerlev@sitronics.com>
2022-07-21 17:50:18 +0300

    ACTS_ON_DETECTED_DEFECTS

---
+++ src/main/java/ru/sitronics/tn/document/model/ActsOnDetectedDefects.java
@@ -0,0 +1,24 @@
+package ru.sitronics.tn.document.model;
+
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@DiscriminatorValue("ACTS_ON_DETECTED_DEFECTS")
+
+public class ActsOnDetectedDefects extends Document {
+
+
+
+}
