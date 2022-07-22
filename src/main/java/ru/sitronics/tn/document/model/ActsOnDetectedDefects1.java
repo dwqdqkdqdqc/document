4b14cd0171155f58e3695a294daf0dd0fcb4aaf9
Mazanenko <mazanenko@inbox.ru>
2022-07-22 16:12:56 +0300

    bug fix

--- src/main/java/ru/sitronics/tn/document/model/ActsOnDetectedDefects1.java
+++ src/main/java/ru/sitronics/tn/document/model/ActsOnDetectedDefects1.java
@@ -1,22 +1,22 @@
-package ru.sitronics.tn.document.model;
-
-
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-import javax.persistence.DiscriminatorValue;
-import javax.persistence.Entity;
-
-
-@Entity
-@Getter
-@Setter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-@DiscriminatorValue("ACTS_OF_DETECTED_DEFECTS_OR_PRODUCT_FAILURES_WITHIN_THE_SCOPE_OF_THE_WARRANTY_CASE")
-
-public class ActsOnDetectedDefects1 extends Document {
-
-
-}
+//package ru.sitronics.tn.document.model;
+//
+//
+//import lombok.AccessLevel;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//import lombok.Setter;
+//
+//import javax.persistence.DiscriminatorValue;
+//import javax.persistence.Entity;
+//
+//
+//@Entity
+//@Getter
+//@Setter
+//@NoArgsConstructor(access = AccessLevel.PROTECTED)
+//@DiscriminatorValue("ACTS_OF_DETECTED_DEFECTS_OR_PRODUCT_FAILURES_WITHIN_THE_SCOPE_OF_THE_WARRANTY_CASE")
+//
+//public class ActsOnDetectedDefects1 extends Document {
+//
+//
+//}
